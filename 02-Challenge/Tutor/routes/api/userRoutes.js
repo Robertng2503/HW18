fa80e6bdaff1461cc751d67d0e0a6993b88cfaca
Robert Nguyen <robertng.thien@gmail.com>
2023-09-13 22:03:41 -0700

    HW18

--- 02-Challenge/Tutor/routes/api/userRoutes.js
+++ 02-Challenge/Tutor/routes/api/userRoutes.js
@@ -2,25 +2,53 @@ const router = require("express").Router();
 const {User} = require("../../models")
 
 router.get("/", async (req, res) => {
-    return await User.find();
+    res.json(await User.find())
 })
 router.get("/:id", async (req, res) => {
-    return await User.findById(req.params.id)
+    try {
+        const users = await User.findById(req.params.id)
+        console.log(users)
+        res.json(users)
+    } catch (error) {
+        console.log(error)
+        res.json(null)
+    }
 })
 router.post("/", async (req, res) => {
-    return await User.create({
+    try{
+        const users = await User.create({
         username: req.body.username,
         email: req.body.email
     })
+    console.log(users)
+        res.json(users)
+    } catch (error) {
+        console.log(error)
+        res.json(null)
+    }
 })
 router.put("/:id", async (req, res) => {
-    return await User.findByIdAndUpdate(req.params.id, {
+    try{
+    const users= await User.findByIdAndUpdate(req.params.id, {
         username: req.body.username,
         email: req.body.email
     })
+    console.log(users)
+        res.json(users)
+    } catch (error) {
+        console.log(error)
+        res.json(null)
+    }
 })
 router.delete("/:id", async (req, res) => {
-        return await User.findByIdAndDelete(req.params.id)
+        try{
+    const users = await User.findByIdAndDelete(req.params.id)
+    console.log(users)
+        res.json(users)
+    } catch (error) {
+        console.log(error)
+        res.json(null)
+    }
 })
 
 
